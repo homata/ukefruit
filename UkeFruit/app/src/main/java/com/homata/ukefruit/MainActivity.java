97b84b48370d0e6dde7969d0084e5256e5c9e239
Hiroshi Omata <op.homata@gmail.com>
2015-01-02 16:01:08 +0900

    WebViewで動作するように変更

--- UkeFruit/app/src/main/java/com/homata/ukefruit/MainActivity.java
+++ UkeFruit/app/src/main/java/com/homata/ukefruit/MainActivity.java
@@ -1,17 +1,34 @@
 package com.homata.ukefruit;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView  myWebView = (WebView)findViewById(R.id.webView1);
+        //標準ブラウザをキャンセル
+        myWebView.setWebViewClient(new WebViewClient());
+        //アプリ起動時に読み込むURL
+        myWebView.loadUrl("http://dev.openfab.jp/~homata/furit/index.html");
+        //myWebView.loadUrl("file:///android_asset/index.html");
+
+        // JavaScript有効
+        myWebView.getSettings().setUseWideViewPort(true);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+
+        // スライドバー無効
+        myWebView.getSettings().setLoadWithOverviewMode(true);
+        myWebView.setScrollBarStyle(WebView.SCROLLBARS_INSIDE_OVERLAY);
     }
 
 
