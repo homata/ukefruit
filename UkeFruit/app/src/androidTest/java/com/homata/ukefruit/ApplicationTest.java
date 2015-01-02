d78775daaf4b3944577536d7caee8f1c39ec6c4f
Hiroshi Omata <op.homata@gmail.com>
2015-01-02 15:40:52 +0900

    change work directory and module path

--- UkeFruit/app/src/androidTest/java/com/homata/ukefruit/ApplicationTest.java
+++ UkeFruit/app/src/androidTest/java/com/homata/ukefruit/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.homata.ukefruit.ukefruit;
+package com.homata.ukefruit;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- UkeFruit/app/src/main/java/com/homata/ukefruit/MainActivity.java
+++ UkeFruit/app/src/main/java/com/homata/ukefruit/MainActivity.java
@@ -1,4 +1,4 @@
-package com.homata.ukefruit.ukefruit;
+package com.homata.ukefruit;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
